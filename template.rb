3b5d37aa4941ce83619334f8a70616c7412612ba
Eliot Jordan <eliotj@princeton.edu>
2023-04-11 11:20:40 -0500

    Add railsrc file and update template.rb

--- template.rb
+++ template.rb
@@ -1,5 +1,11 @@
 gem "dls_template"
 
 run "bundle install"
-
 generate "dls_template:install", "-f"
+
+# Remove unneeded dls_template gem from Gemfile.
+gemfile = File.expand_path("Gemfile")
+File.write(gemfile, File.open(gemfile) do |f|
+                      text = f.read
+                      text.gsub(/^gem "dls_template"$/, "")
+                    end)
