7a598d28f2e4a49744a75ac5b33d0cc0f930ed90
Eliot Jordan <eliotj@princeton.edu>
2023-04-14 12:16:40 -0500

    Add task to generate test rails app

--- Rakefile
+++ Rakefile
@@ -1,3 +1,13 @@
-# frozen_string_literal: true
-
 require "bundler/gem_tasks"
+require "fileutils"
+require "./lib/dls_template"
+
+desc "Generate test app"
+task :generate do
+  test_app_dir = File.join(DlsTemplate.root, ".test_app")
+  FileUtils.rm_rf(test_app_dir) if Dir.exist?(test_app_dir)
+  system "GEM_TEST=true rails new --skip-bundle --no-rc .test_app -m template.rb"
+  generator = DlsTemplate::Install.new
+  generator.destination_root = test_app_dir
+  generator.invoke_all
+end
--- template.rb
+++ template.rb
@@ -8,12 +8,14 @@ gem_group :development, :test do
   gem "bixby"
 end
 
-run "bundle install"
-generate "dls_template:install", "-f"
+unless ENV["GEM_TEST"]
+  run "bundle install"
+  generate "dls_template:install", "-f"
 
-# Remove unneeded dls_template gem from Gemfile.
-gemfile = File.expand_path("Gemfile")
-File.write(gemfile, File.open(gemfile) do |f|
-                      text = f.read
-                      text.gsub(/^gem "dls_template"$/, "")
-                    end)
+  # Remove unneeded dls_template gem from Gemfile.
+  gemfile = File.expand_path("Gemfile")
+  File.write(gemfile, File.open(gemfile) do |f|
+                        text = f.read
+                        text.gsub(/^gem "dls_template"$/, "")
+                      end)
+end
